7418d95093139e00db7642473015bbb227223701
jase-d-ace <andrada.jasonp@gmail.com>
2020-12-08 16:52:52 -0500

    create state placeholders and receive data from api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import { callAPI, handleFormSubmit, handleFormInput } from './services';
+import { handleFormSubmit, handleFormInput } from './services';
 
 function App() {
   const [locationString, setLocationString] = useState(null);
-  console.log(locationString)
+  const [isLoading, setIsLoading] = useState(false);
+  const [responseData, setResponseData] = useState(null);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <form onSubmit={(e) => handleFormSubmit(e, locationString, callAPI)}>
+      <form onSubmit={(e) => handleFormSubmit(e, locationString, setResponseData)}>
         <input type="text" placeholder="enter location" onChange={(e) => handleFormInput(setLocationString, e.target.value)} />
         <input type="submit" value="Click me" /> 
       </form>
