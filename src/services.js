2f46430dfb0bde1c4db57a9cf40c9c9ddef08781
jase-d-ace <andrada.jasonp@gmail.com>
2020-12-08 17:32:16 -0500

    Services: Add check for error in api call

--- src/services.js
+++ src/services.js
@@ -5,13 +5,18 @@ export const handleFormSubmit = (e, locationString, callback) => {
   e.preventDefault()
   fetch(`${API_BASE_URL}locations=${locationString}&aggregateHours=1&unitGroup=us&forecastDays=8&shortColumnNames=false&contentType=json&key=${process.env.REACT_APP_API_KEY}`)
   .then(data => data.json())
-  .then(({locations}) => {
-    // results from this api are bound to a dynamically generated key that is equal to whatever the location the user searched for, e.g. "troy,ny" or a pair of lon/lat coordinates
-    let locationKey = Object.keys(locations)[0];
-    const { address, values, currentConditions } = locations[locationKey];
-    callback({address, values, currentConditions})
+  .then((json) => {
+    if (json.locations) {
+      const { locations } = json
+      // results from this api are bound to a dynamically generated key that is equal to whatever the location the user searched for, e.g. "troy,ny" or a pair of lon/lat coordinates
+      let locationKey = Object.keys(locations)[0];
+      const { address, values, currentConditions } = locations[locationKey];
+      callback({ address, values, currentConditions })
+    } else {
+      callback(json)
+    }
   })
-  .catch(err => err)
+  .catch(err => callback(err))
 }
 
 export const handleFormInput = (callback, callbackArg) => (_.debounce(() => callback(callbackArg), 70))()
