b4b0e3492e408ac3a06c6f2f4a2700151d1e82fa
jase-d-ace <andrada.jasonp@gmail.com>
2020-12-08 17:32:46 -0500

    Root: add state to save error in case of no location data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { handleFormSubmit, handleFormInput } from './services';
 
 function App() {
   const [locationString, setLocationString] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const [responseData, setResponseData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (responseData) {
+      if (!responseData.address) {
+        setResponseData(null);
+        setError(responseData);
+      }
+    }
+  })
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <form onSubmit={(e) => handleFormSubmit(e, locationString, setResponseData)}>
+      <form onSubmit={(e) =>handleFormSubmit(e, locationString, setResponseData)}>
         <input type="text" placeholder="enter location" onChange={(e) => handleFormInput(setLocationString, e.target.value)} />
         <input type="submit" value="Click me" /> 
       </form>
