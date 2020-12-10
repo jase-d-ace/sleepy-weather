3c61b21c4d3e8ca4662976a6a891a27deb7a7876
jase-d-ace <andrada.jasonp@gmail.com>
2020-12-09 22:57:28 -0500

    Services: move data cleaning to api call and fix spacing

--- src/services.js
+++ src/services.js
@@ -1,21 +1,22 @@
 const API_BASE_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?"
 
 export const handleFormSubmit = (e, locationString, callback) => {
-  e.preventDefault()
-  fetch(`${API_BASE_URL}locations=${locationString}&aggregateHours=1&unitGroup=us&forecastDays=8&shortColumnNames=false&contentType=json&key=${process.env.REACT_APP_API_KEY}`)
-  .then(data => data.json())
-  .then((json) => {
-    if (json.locations) {
-      const { locations } = json
-      // results from this api are bound to a dynamically generated key that is equal to whatever the location the user searched for, e.g. "troy,ny" or a pair of lon/lat coordinates
-      let locationKey = Object.keys(locations)[0];
-      const { address, values, currentConditions } = locations[locationKey];
-      callback({ address, values, currentConditions })
-    } else {
-      callback(json)
-    }
-  })
-  .catch(err => callback(err))
+    e.preventDefault()
+    fetch(`${API_BASE_URL}locations=${locationString}&aggregateHours=1&unitGroup=us&forecastDays=8&shortColumnNames=false&contentType=json&key=${process.env.REACT_APP_API_KEY}`)
+        .then(data => data.json())
+        .then((json) => {
+            if (json.locations) {
+                const { locations } = json
+                // results from this api are bound to a dynamically generated key that is equal to whatever the location the user searched for, e.g. "troy,ny" or a pair of lon/lat coordinates
+                let locationKey = Object.keys(locations)[0];
+                const { address, values, currentConditions } = locations[locationKey];
+                let relevantTemps = values.filter(({ datetimeStr }) => new Date(datetimeStr) <= new Date(datetimeStr).setHours(6, 0, 0) || (new Date(datetimeStr) >= new Date(datetimeStr).setHours(18, 0, 0) && new Date(datetimeStr) <= new Date(datetimeStr).setHours(23, 0, 0)))
+                callback({ address, relevantTemps, currentConditions })
+            } else {
+                callback(json)
+            }
+        })
+        .catch(err => callback(err))
 }
 
 export const handleFormInput = (callback, callbackArg) => callback(callbackArg)
