cc9afef19de106184bfa4b2225a6b913a612070c
jase-d-ace <andrada.jasonp@gmail.com>
2020-12-21 18:14:15 -0500

    Daily Card: Render props

--- src/components/DailyCard/DailyCard.js
+++ src/components/DailyCard/DailyCard.js
@@ -1,17 +1,27 @@
 import HourlyRow from 'components/HourlyRow/HourlyRow'
+import { getDay, getDate, getMonth } from '../../services'
 
 
-function DailyCard({ overnights }) {
+function DailyCard({ high, low, day }) {
     return (
         <div className="DailyCard">
-            <h1>
-                We have a right column!
-            </h1>
-            <ul>
+            <div className="DailyCard__summary">
+                <div className="summary-date">
+                    {getDay(day)}, {getMonth(day)} {getDate(day)}
+                </div>
+                <div className="summary-overnights">
+                    Overnight: {high}/{low}
+                </div>
+
+            </div>
+            <div className="summary-advice">
+                It's gonna be chilly, break out the fuzzies and comforter!
+                </div>
+            {/* <ul>
                 {
                     overnights.map(night => < HourlyRow key={night["day"]} {...night} />)
                 }
-            </ul>
+            </ul> */}
         </div>
 
     )
