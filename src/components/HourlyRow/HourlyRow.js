fe8f0e9d2e1153c37589fcd9c24337d8ee2de00d
jase-d-ace <andrada.jasonp@gmail.com>
2020-12-24 15:09:59 -0500

    Hourly Row: render data in hourly row component

--- src/components/HourlyRow/HourlyRow.js
+++ src/components/HourlyRow/HourlyRow.js
@@ -1,13 +1,28 @@
 import { getMonth, getDay, getDate } from "services";
 // const [hourlyWeather, setHourlyWeather] = useState(null)
 
-function HourlyRow({ high, low, day }) {
+function HourlyRow({ datetime, temp, conditions }) {
     return (
-        <li>
-            <p>
-                {getDay(day)}, {getMonth(day)}, {getDate(day)}
-            </p>
-            Overnight High: {high} Degrees, Overnight Low: {low} Degrees
+        <li className="DailyCard__hourly">
+            <div className="hour">
+                <div>
+                    {new Date(datetime).getHours() > 12 ?
+                        (`${new Date(datetime).getHours() - 12}pm`) :
+                        new Date(datetime).getHours() === 0 ? (`12am`) :
+                            (`${new Date(datetime).getHours()}am`)}
+                </div>
+                <div>
+                    {temp}
+                </div>
+                <div>
+                    {conditions}
+                </div>
+                <div>
+                    🌨️
+                    </div>
+            </div>
+
+
         </li>
     );
 }
