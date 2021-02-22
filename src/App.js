1c0810215b82c685dbd1f0f8230b6815e7004951
jase-d-ace <andrada.jasonp@gmail.com>
2021-02-22 18:49:44 -0500

    Root: add a div that is only visible on mobile

--- src/App.js
+++ src/App.js
@@ -135,6 +135,9 @@ function App() {
                     </div>
                 </div>
             </div>
+            <div className="WeatherApp__Mobile">
+                <h1>I RENDER ONLY ON MOBILE</h1>
+            </div>
         </div>
     );
 }
